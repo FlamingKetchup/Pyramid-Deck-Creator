7fda84274c746ff59a13e7cff8edd154f18a8031
FlamingKetchup <43097488+FlamingKetchup@users.noreply.github.com>
2020-07-13 21:30:39 -0700

    Got card addition and removal working

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-var textField = document.createElement("input");
-var imageUpload = document.createElement("input");
-textField.type = "text";
-textField.placeholder = "Card text";
-textField.setAttribute('maxlength', '50')
-imageUpload.type = "file";
-imageUpload.accept = "image/*";
-var characters = document.getElementById('characters');
-characters.appendChild(textField);
-characters.appendChild(imageUpload);
-characters.appendChild(document.createElement("br"))
+function addCard(targetList) {
+  var div = document.createElement("div");
+  var textField = document.createElement("input");
+  var imageUpload = document.createElement("input");
+  var removeButton = document.createElement("button")
+
+  textField.type = "text";
+  textField.placeholder = "Card text";
+  textField.setAttribute("maxlength", "62");
+  imageUpload.type = "file";
+  imageUpload.accept = "image/*";
+  removeButton.type = "button";
+  removeButton.onclick = function() {
+    return this.parentNode.remove();
+  }
+  removeButton.appendChild(document.createTextNode("Remove"));
+
+  var cardList = document.getElementById(targetList);
+  var button = document.getElementById(targetList + '_add');
+
+  cardList.insertBefore(div, button)
+  div.appendChild(textField);
+  div.appendChild(imageUpload);
+  div.appendChild(removeButton)
+  div.appendChild(document.createElement("br"));
+}
+
+function removeParent() {
+  if (this.parentNode != undefined) {
+    this.parentNode.removeChild();
+  }
+}
