2bfead5ef593d4a5bc3d151ec8ef1aae728b6c74
FlamingKetchup <43097488+FlamingKetchup@users.noreply.github.com>
2020-07-14 13:44:38 -0700

    Changed styling, inputs are now put in alerts for testing

--- script.js
+++ script.js
@@ -7,26 +7,62 @@ function addCard(targetList) {
   textField.type = "text";
   textField.placeholder = "Card text";
   textField.setAttribute("maxlength", "62");
+  textField.name = targetList + "TextField";
   imageUpload.type = "file";
   imageUpload.accept = "image/*";
+  imageUpload.name = targetList + "ImageUpload";
   removeButton.type = "button";
   removeButton.onclick = function() {
     return this.parentNode.remove();
   }
-  removeButton.appendChild(document.createTextNode("Remove"));
+  removeButton.appendChild(document.createTextNode("Delete"));
 
   var cardList = document.getElementById(targetList);
   var button = document.getElementById(targetList + '_add');
 
-  cardList.insertBefore(div, button)
+  cardList.insertBefore(div, button);
   div.appendChild(textField);
   div.appendChild(imageUpload);
-  div.appendChild(removeButton)
+  div.appendChild(removeButton);
   div.appendChild(document.createElement("br"));
 }
 
-function removeParent() {
-  if (this.parentNode != undefined) {
-    this.parentNode.removeChild();
+function generateImage() {
+  var form = document.getElementById("form");
+  var charactersText = [];
+  var charactersImages = [];
+  var challengesText = [];
+  var challengesImages = [];
+
+  for (i of form.elements) {
+    switch (i.name) {
+      case "charactersTextField":
+        charactersText.push(i.value);
+        break;
+      case "charactersImageUpload":
+        charactersImages.push(i.value);
+        break;
+      case "challengesTextField":
+        challengesText.push(i.value);
+        break;
+      case "challengesImageUpload":
+        challengesImages.push(i.value);
+        break;
+      case "borderColor":
+        var borderColor = i.value;
+        break;
+      case "charactersIcon":
+        var charactersIcon = i.value;
+        break;
+      case "challengesIcon":
+        var challengesIcon = i.value;
+    }
   }
+  alert(charactersText);
+  alert(charactersImages);
+  alert(challengesText);
+  alert(challengesImages);
+  alert(borderColor);
+  alert(charactersIcon);
+  alert(challengesIcon);
 }
