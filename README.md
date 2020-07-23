6dc5e45e1d002cf12a822a45eac1ae87d02854b3
FlamingKetchup <43097488+FlamingKetchup@users.noreply.github.com>
2020-07-23 15:24:07 -0700

    Added Cropping

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pyramid Deck Creator
 Online deck creator for the Pyramid. Use it at https://flamingketchup.github.io/Pyramid-Deck-Creator
+
+Uses cropper.js: https://github.com/fengyuanchen/cropperjs
--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 const background = document.createElement("img");
 background.src = "card_background.png";
+var modalBackground, modal, modalImage, modalButton;
+window.onload = function () {
+  modalBackground = document.getElementById("cropModalBackground");
+  modal = document.getElementById("cropModal");
+  modalImage = document.getElementById("modalImage");
+  modalButton = document.getElementById("modalButton");
+}
+Cropper.setDefaults({viewMode: 1, minContainerWidth: 1, minContainerHeight: 1, restore: false, autoCropArea: 1, toggleDragModeOnDblclick: false, cropBoxMovable: false});
+
+//Takes the file and puts it into the image
+function readImage(file, image) {
+  const reader = new FileReader();
+  reader.onload = function () {
+    image.src = reader.result;
+  }
+  reader.readAsDataURL(file);
+}
+
+//Takes the file, crops it, and returns how to clip it
+function cropImage(file, writeTarget) {
+  const reader = new FileReader();
+  reader.onload = function () {
+    modalImage.src = reader.result;
+    modalImage.onload = function () {
+      const cropper = new Cropper(modalImage, {ready () {
+        modalButton.onclick = function () {
+          writeTarget.dataset.cropX = cropper.getData({rounded: true}).x;
+          writeTarget.dataset.cropY = cropper.getData({rounded: true}).y;
+          writeTarget.dataset.width = cropper.getData({rounded: true}).width;
+          writeTarget.dataset.height = cropper.getData({rounded: true}).height;
+          cropper.destroy();
+          modalBackground.style.display = "none";
+        }
+      }});
+      modalBackground.style.display = "block";
+    }
+  }
+  reader.readAsDataURL(file);
+}
 
 //Adds a text and a file input to the target container
 function addCard(targetList) {
   const div = document.createElement("div");
   const textField = document.createElement("input");
   const imageUpload = document.createElement("input");
-  const removeButton = document.createElement("button")
+  const removeButton = document.createElement("button");
 
   textField.type = "text";
   textField.placeholder = "Card text";
@@ -15,8 +54,11 @@ function addCard(targetList) {
   imageUpload.type = "file";
   imageUpload.accept = "image/*";
   imageUpload.name = targetList + "ImageUpload";
+  imageUpload.onchange = function () {
+    cropImage(this.files[0], this);
+  }
   removeButton.type = "button";
-  removeButton.onclick = function() {
+  removeButton.onclick = function () {
     return this.parentNode.remove();
   }
   removeButton.appendChild(document.createTextNode("Delete"));
@@ -31,15 +73,6 @@ function addCard(targetList) {
   div.appendChild(document.createElement("br"));
 }
 
-//Takes the file and puts it into the image
-function readImage(file, image) {
-  const reader = new FileReader();
-  reader.onload = function () {
-    image.src = reader.result;
-  }
-  reader.readAsDataURL(file);
-}
-
 //Renders the cards onto the canvas element with id="preview"
 function generateImage() {
   const form = document.getElementById("form");
@@ -47,10 +80,14 @@ function generateImage() {
   var charactersImages = [];
   var challengesText = [];
   var challengesImages = [];
+  var challengesImagesCropData = [];
 
   //Organizing user input
   for (i of form.elements) {
     switch (i.name) {
+      case "borderColor":
+        var borderColor = i.value;
+        break;
       case "charactersTextField":
         charactersText.push(i.value);
         break;
@@ -58,19 +95,16 @@ function generateImage() {
         challengesText.push(i.value);
         break;
       case "charactersImageUpload":
-        charactersImages.push(i.files[0]);
+        charactersImages.push({image: i.files[0], width: i.dataset.width, cropX: i.dataset.cropX, cropY: i.dataset.cropY, height: i.dataset.height});
         break;
       case "challengesImageUpload":
-        challengesImages.push(i.files[0]);
-        break;
-      case "borderColor":
-        var borderColor = i.value;
+        challengesImages.push({image: i.files[0], width: i.dataset.width, cropX: i.dataset.cropX, cropY: i.dataset.cropY, height: i.dataset.height});
         break;
       case "charactersIcon":
-        var charactersIcon = i.files[0];
+        var charactersIcon = {image: i.files[0], width: i.dataset.width, cropX: i.dataset.cropX, cropY: i.dataset.cropY, height: i.dataset.height};
         break;
       case "challengesIcon":
-        var challengesIcon = i.files[0];
+        var challengesIcon = {image: i.files[0], width: i.dataset.width, cropX: i.dataset.cropX, cropY: i.dataset.cropY, height: i.dataset.height};
     }
   }
 
@@ -109,27 +143,27 @@ function generateImage() {
   //Process image files into images
   var tempImage;
   for (var i = 0; i < images.length; i++) {
-    if (images[i] !== undefined) {
-      tempFile = images[i];
-      images[i] = document.createElement("img");
-      readImage(tempFile, images[i]);
+    if (images[i].image !== undefined) {
+      tempFile = images[i].image;
+      images[i].image = document.createElement("img");
+      readImage(tempFile, images[i].image);
     }
   }
 
-  if (charactersIcon !== undefined) {
-    tempFile = charactersIcon;
-    charactersIcon = document.createElement("img");
-    readImage(tempFile, charactersIcon);
+  if (charactersIcon.image !== undefined) {
+    tempFile = charactersIcon.image;
+    charactersIcon.image = document.createElement("img");
+    readImage(tempFile, charactersIcon.image);
   }
 
-  if (challengesIcon !== undefined) {
-    tempFile = challengesIcon;
-    challengesIcon = document.createElement("img");
-    readImage(tempFile, challengesIcon);
+  if (challengesIcon.image !== undefined) {
+    tempFile = challengesIcon.image;
+    challengesIcon.image = document.createElement("img");
+    readImage(tempFile, challengesIcon.image);
   }
 
   //Actually rendering cards
-  for (var i = 0; i < text.length; i++) {
+  for (let i = 0; i < text.length; i++) {
     let posX = i % 10 * 410;
     let posY = Math.floor(i / 10) * 584;
     c.drawImage(background, posX, posY);
@@ -142,22 +176,22 @@ function generateImage() {
       c.strokeText(text[i][j], posX + 203, posY + 55 + j * 48);
       c.fillText(text[i][j], posX + 203, posY + 55 + j * 48);
     }
-    if (images[i] !== undefined) {
-      images[i].onload = function() {
-        c.drawImage(this, posX + 138, posY + 328, 130, 130);
+    if (images[i].image !== undefined) {
+      images[i].image.onload = function() {
+        c.drawImage(images[i].image, images[i].cropX, images[i].cropY, images[i].width, images[i].height, posX + 138, posY + 328, 130, 130);
       }
     }
-    if (i < charactersText.length && charactersIcon !== undefined) {
-      charactersIcon.addEventListener("load", function() {
+    if (i < charactersText.length && charactersIcon.image !== undefined) {
+      charactersIcon.image.addEventListener("load", function() {
         c.filter = "grayscale(100%)";
-        c.drawImage(charactersIcon, posX + 347, posY + 526, 38, 38);
+        c.drawImage(charactersIcon.image, charactersIcon.cropX, charactersIcon.cropY, charactersIcon.width, charactersIcon.height, posX + 347, posY + 526, 38, 38);
         c.filter = "none";
       });
     }
-    else if (challengesIcon !== undefined) {
-      challengesIcon.addEventListener("load", function() {
+    else if (challengesIcon.image !== undefined) {
+      charactersIcon.image.addEventListener("load", function() {
         c.filter = "grayscale(100%)";
-        c.drawImage(challengesIcon, posX + 347, posY + 526, 38, 38)
+        c.drawImage(challengesIcon.image, challengesIcon.cropX, challengesIcon.cropY, challengesIcon.width, challengesIcon.height, posX + 347, posY + 526, 38, 38);
         c.filter = "none";
       });
     }
